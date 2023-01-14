f206dc15aab6a8875c06b4329085f62af72553aa
Lily <hayashi.quan@gmail.com>
2023-01-14 15:46:35 +0100

    fix - remove dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-/** @type {import('next').NextConfig} */
-
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-});
-
-module.exports = withMDX({
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+module.exports = {
     reactStrictMode: true,
     swcMinify: true,
-});
+};
