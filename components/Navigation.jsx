810bb67ff259f71e76a6e1a249075e36eea83164
Lily <hayashi.quan@gmail.com>
2023-01-23 14:13:30 +0100

    feat - menu responsive

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import categories from '../contexts/categories.json';
 
 const Navigation = () => {
+    const handleMobileMenu = () => {
+        const el = document.querySelector('#mobile-menu');
+        el.classList.toggle('active');
+        document.body.style.overflowY = el.classList.contains('active') ? 'hidden' : 'unset';
+    };
+
     return (
         <S.Container>
             <S.Logo href='/'>Lily Dev</S.Logo>
@@ -14,17 +20,51 @@ const Navigation = () => {
                     <S.CategoriesContainer>
                         <S.Categories>
                             {Object.entries(categories).map(([key, value]) => (
-                                <Link  key={key} href={`/posts/category/${key.toLowerCase()}`}>{value.name}</Link>
+                                <Link key={key} href={`/posts/category/${key.toLowerCase()}`}>
+                                    {value.name}
+                                </Link>
                             ))}
                         </S.Categories>
                     </S.CategoriesContainer>
                 </S.CategoryLabel>
             </S.Links>
             <S.Search>
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
-                    <path d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z' />
-                </svg>
+                <S.Icon>
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+                        <path d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z' />
+                    </svg>
+                </S.Icon>
             </S.Search>
+            <S.MobileMenu>
+                <S.Icon onClick={handleMobileMenu}>
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+                        <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+                    </svg>
+                </S.Icon>
+                <S.MobileMenuContent id='mobile-menu'>
+                    <S.MobileMenuHeader>
+                        <S.Logo onClick={handleMobileMenu} href='/'>
+                            Lily Dev
+                        </S.Logo>
+                        <S.Icon onClick={handleMobileMenu}>
+                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'>
+                                <path d='M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z' />
+                            </svg>
+                        </S.Icon>
+                    </S.MobileMenuHeader>
+                    <Link onClick={handleMobileMenu} href='/posts/late'>
+                        Les late
+                    </Link>
+                    <span>Les catégories</span>
+                    <S.MobileCategories>
+                        {Object.entries(categories).map(([key, value]) => (
+                            <Link onClick={handleMobileMenu} key={key} href={`/posts/category/${key.toLowerCase()}`}>
+                                {value.name}
+                            </Link>
+                        ))}
+                    </S.MobileCategories>
+                </S.MobileMenuContent>
+            </S.MobileMenu>
         </S.Container>
     );
 };
@@ -37,6 +77,10 @@ S.Container = styled.div`
     align-items: center;
     width: 100%;
     margin-bottom: 100px;
+
+    @media (max-width: 750px) {
+        margin-bottom: 50px;
+    }
 `;
 
 S.Logo = styled(Link)`
@@ -66,6 +110,10 @@ S.Links = styled.div`
             left: 0;
             z-index: -1;
         }
+    }
+
+    @media (max-width: 750px) {
+        display: none;
     }
 `;
 
@@ -102,7 +150,6 @@ S.CategoriesContainer = styled.div`
 
 S.Categories = styled.div`
     background: #ffffff24;
-    gap: 4px 0;
     padding: 10px;
     border: ${({ theme }) => theme.border};
     display: grid;
@@ -143,5 +190,81 @@ S.Search = styled.div`
     & > svg {
         fill: #fff;
         width: 15px;
+    }
+
+    @media (max-width: 750px) {
+        display: none;
+    }
+`;
+
+S.Icon = styled.div`
+    margin-left: auto;
+    background: ${({ theme }) => theme.primary};
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 35px;
+    height: 33px;
+
+    & svg {
+        fill: #fff;
+        width: 15px;
+    }
+`;
+
+// MOBILE MENU ---------------------------------------------
+S.MobileMenu = styled.div`
+    margin-left: auto;
+
+    @media (min-width: 750px) {
+        display: none;
+    }
+`;
+
+S.MobileMenuContent = styled.div`
+    display: none;
+    flex-direction: column;
+    background: ${({ theme }) => theme.background};
+    z-index: 100;
+    position: fixed;
+    inset: 0;
+    padding: 25px 40px;
+
+    & > span {
+        color: ${({ theme }) => theme.primary};
+        margin-bottom: 25px;
+        font-size: 21px;
+    }
+    & a {
+        font-size: 21px;
+    }
+    & > a {
+        margin-bottom: 50px;
+    }
+
+    &.active {
+        display: flex;
+    }
+`;
+
+S.MobileMenuHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 75px;
+
+    & a {
+        font-size: 24px;
+    }
+`;
+
+S.MobileCategories = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 25px 40px;
+
+    & a {
+        font-size: 17px;
     }
 `;
