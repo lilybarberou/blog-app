b3d9ecb1bd6a4803f2eb27a11be4649ea9a95759
Lily <hayashi.quan@gmail.com>
2023-01-28 16:03:12 +0100

    fix - ssr styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default function Document() {
     return (
@@ -26,4 +27,29 @@ export default function Document() {
             </body>
         </Html>
     );
+}
+
+export async function getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    {sheet.getStyleElement()}
+                </>
+            ),
+        };
+    } finally {
+        sheet.seal();
+    }
 }
