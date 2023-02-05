fca4415447f8ea7b198f598b608ed95810167423
Lily <hayashi.quan@gmail.com>
2023-02-05 21:48:26 +0100

    fix - contrast color

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ const globalTheme = {
     background: '#25262C',
     primary: '#C64141',
     secondary: '#c5a186',
-    grey: '#878787',
+    grey: '#8F8F8F',
     border: '1px solid #8D8D8D',
 };
 
