1a94306eea0ba14bf9c2f39705a93750930209a9
Lily <hayashi.quan@gmail.com>
2023-08-13 20:31:58 +0200

    add - search

---
+++ components/ModalSearch.jsx
@@ -0,0 +1,138 @@
+import axios from 'axios';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const ModalSearch = ({ onClose }) => {
+    const [results, setResults] = useState({ initial: [], filtered: [] });
+
+    // get all files
+    useEffect(() => {
+        const getFiles = async () => {
+            const { data } = await axios.get('files');
+            const arr = [
+                ...data.data.posts.map((post) => ({ ...post, type: 'posts' })),
+                ...data.data.snippets.map((snippet) => ({ ...snippet, type: 'snippets' })),
+            ];
+
+            setResults({ initial: arr, filtered: arr });
+        };
+
+        getFiles();
+    }, []);
+
+    // handle search change
+    const handleChange = (e) => {
+        const { value } = e.target;
+        if (!value) return setResults((prev) => ({ ...prev, filtered: prev.initial }));
+
+        const filtered = results.initial.filter((result) => result.title.toLowerCase().includes(value.toLowerCase()));
+        setResults((prev) => ({ ...prev, filtered }));
+    };
+
+    return (
+        <>
+            <S.Modal>
+                <S.Content>
+                    <S.Header>
+                        <S.Input autoFocus type='text' onChange={handleChange} />
+                        <S.Icon onClick={onClose}>
+                            <svg width='24px' height='24px' strokeWidth='1.5' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' color='#fff'>
+                                <path
+                                    d='M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243'
+                                    stroke='#fff'
+                                    strokeWidth='1.5'
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                ></path>
+                            </svg>
+                        </S.Icon>
+                    </S.Header>
+                    {results.filtered.map((result) => (
+                        <S.Result onClick={onClose} key={result.title} href={`/${result.type}/${result.slug}`}>
+                            <p>{result.title}</p>
+                        </S.Result>
+                    ))}
+                </S.Content>
+            </S.Modal>
+            <S.Background onClick={onClose} />
+        </>
+    );
+};
+
+export default ModalSearch;
+
+const S = {};
+S.Modal = styled.form`
+    z-index: 100;
+    border: 1px solid #fff;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #1d1e23;
+    width: 400px;
+    height: 70vh;
+`;
+
+S.Content = styled.div`
+    overflow-y: scroll;
+    max-height: calc(100% - 40px);
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`;
+
+S.Header = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 40px;
+    align-items: center;
+    gap: 15px;
+    height: 40px;
+`;
+
+S.Input = styled.input`
+    padding: 10px;
+    background: none;
+    border: 1px solid #fff;
+    color: #fff;
+    outline: none;
+    height: 100%;
+    box-sizing: border-box;
+    font-size: 15px;
+`;
+
+S.Icon = styled.div`
+    margin-left: auto;
+    background: ${({ theme }) => theme.primary};
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+
+    svg {
+        fill: #fff;
+        width: 30px;
+    }
+`;
+
+S.Result = styled(Link)`
+    padding: 10px;
+    border-radius: 5px;
+    transition: 0.2s;
+    background: #2d2d35;
+
+    &:hover {
+        color: ${({ theme }) => theme.primary};
+    }
+`;
+
+S.Background = styled.div`
+    z-index: 99;
+    position: fixed;
+    inset: 0;
+    background: rgba(0, 0, 0, 0.5);
+`;
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 import categories from '@contexts/categories.json';
+import useModal from '@contexts/useModal';
+import ModalSearch from './ModalSearch';
 
 const Navigation = () => {
+    const { isOpen, open, close } = useModal(['search']);
+
     const handleMobileMenu = () => {
         const el = document.querySelector('#mobile-menu');
         el.classList.toggle('active');
@@ -11,6 +15,7 @@ const Navigation = () => {
 
     return (
         <S.Container>
+            {isOpen('search') && <ModalSearch onClose={() => close('search')} />}
             <S.Logo href='/'>LilyScript</S.Logo>
             <S.Links>
                 <Link href='/posts'>Les posts</Link>
@@ -29,7 +34,7 @@ const Navigation = () => {
                 </S.CategoryLabel>
                 <Link href='/snippets'>Les snippets</Link>
             </S.Links>
-            <S.Search>
+            <S.Search onClick={() => open('search')}>
                 <S.Icon>
                     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
                         <path d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z' />
@@ -195,8 +200,8 @@ S.Search = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 35px;
-    height: 33px;
+    width: 38px;
+    height: 34px;
 
     & > svg {
         fill: #fff;
@@ -215,8 +220,8 @@ S.Icon = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 35px;
-    height: 33px;
+    width: 38px;
+    height: 34px;
 
     & svg {
         fill: #fff;
