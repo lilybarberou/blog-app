d7a3f4f7693eb445910c8b67d646da4ff5829237
Lily <hayashi.quan@gmail.com>
2023-08-15 23:08:27 +0200

    test - revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -3,9 +3,11 @@ import { NextApiRequest, NextApiResponse } from "next"
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const path = req.query?.path as string;
     if (!path) return;
+    console.log(path);
     
     try {
         await res.revalidate(path);
+        console.log('revalitated')
         return res.json({ revalidated: true })
     } catch (err) {
       return res.status(500).send('Error revalidating')
